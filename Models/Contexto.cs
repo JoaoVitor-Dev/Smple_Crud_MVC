ffbcb8db4d8b00ef30c97d8407e7da2928a1efba
João Vitor <joao.vitor@maxdata.com.br>
2024-10-21 17:47:05 -0300

    Finalizado

---
+++ Models/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Smple_Crud_MVC.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Produto> Produto { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Smple_Crud_MVC.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<Contexto>(options => options.UseSqlServer("Server=localhost;Database=SimpleCrudMVC;User Id=sa;Password=macro01;Trusted_Connection=SSPI;Encrypt=false;TrustServerCertificate=true"));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+   
     app.UseHsts();
 }
 
